30999c1c4b908ae4420279c4fc70c43d0e82b477
Vesal Daneshvar <vessaldaneshvar@gmail.com>
2021-03-05 22:11:03 +0330

    add frontend data dynamicly render from backend

--- frontend/src/Bar.js
+++ frontend/src/Bar.js
@@ -1,33 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {Line} from 'react-chartjs-2';
-const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        label: 'قیمت تاکسی در طول زمان',
-        fill: true,
-        lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: [65, 59, 80, 81, 56, 55, 40]
-      }
-    ]
-  };
 
 function App() {
+  const [price, setPrice] = useState([])
+  const [label, setLabel] = useState([])
+  let data = {
+      labels: label,
+      datasets: [
+        {
+          label: 'قیمت تاکسی در طول زمان',
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: price
+        }
+      ]
+    }
+  const get_data = () => {
+    fetch("http://127.0.0.1:8000/prices/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw response;
+      }
+      return response.json();
+    })
+    .then((response) => {
+      setLabel(response.map((item) => item.created_at))
+      setPrice(response.map((item) => item.price))
+    })
+  }
+  useEffect(() => {
+    get_data();
+  }, []);
   return (
     <div>
         <h2>Area Chart</h2>
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional, List
 from fastapi import FastAPI
 from fastapi.params import Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm.session import Session
 
 import crud, schemas
@@ -10,6 +11,20 @@ from database import SessionLocal, engine
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Dependency
 def get_db():
